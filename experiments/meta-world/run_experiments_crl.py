1a8e8d1d7706598cb3e0ed689a998e7af2e6ca28
Okyumi <omoriyumi8@gmail.com>
2026-01-17 11:48:39 +0400

    Refactor argument parsing in run_experiments_crl.py: renamed '--capture-video' to '--track', updated default values, and modified run name format for contrastive RL.

--- experiments/meta-world/run_experiments_crl.py
+++ experiments/meta-world/run_experiments_crl.py
@@ -38,7 +38,8 @@ def parse_args():
     parser.add_argument("--task-id", type=int, default=3)
     parser.add_argument("--tag", type=str, default="Debug")
     parser.add_argument("--debug", type=str2bool, default=False)
-    parser.add_argument("--capture-video", action="store_true")
+    parser.add_argument("--track", action="store_true")
+    parser.add_argument("--capture-video", default=False, action="store_true")
     parser.add_argument("--video-every-n-episodes", type=int, default=50)
     parser.add_argument("--pool_size", default=20)
     parser.add_argument("--encoder_from_base", action="store_true")
@@ -48,7 +49,8 @@ def parse_args():
 args = parse_args()
 
 # modes = list(range(20)) if args.algorithm != "simple" else list(range(10))
-modes = [args.task_id]
+# modes = [args.task_id]
+modes = list(range(10)) 
 # args.start_mode = 3
 if args.algorithm not in ["simple", "packnet", "prognet", "cka-rl", "masknet", "cbpnet", "crelus"] and args.start_mode == 0:
     start_mode = 1
@@ -60,19 +62,21 @@ if start_mode not in modes:
     start_mode = modes[0]  # Use first available task
 
 run_name = (
-    lambda task_id: f"task_{task_id}__{args.algorithm if task_id > 0 or args.algorithm in ['packnet', 'prognet', 'cka-rl', 'masknet', 'cbpnet', 'crelus'] else 'simple'}__run_sac__{args.seed}"
+    lambda task_id: f"task_{task_id}__{args.algorithm if task_id > 0 or args.algorithm in ['packnet', 'prognet', 'cka-rl', 'masknet', 'cbpnet', 'crelus'] else 'simple'}__run_contrastiveRL__{args.seed}"
 )
 
 first_idx = modes.index(start_mode)
 for i, task_id in enumerate(modes[first_idx:]):
     params = f"--model-type={args.algorithm} --task-id={task_id} --seed={args.seed} --tag={args.tag}"
     if args.debug:
-        params += " --total-timesteps=5000"
+        params += " --total-timesteps=10000"
         params += " --learning_starts=5"
     if args.encoder_from_base:
         params += " --encoder-from-base"
     else:
         params += " --no-encoder-from-base"
+    if args.track:
+        params += " --track"
     if args.capture_video:
         params += " --capture-video"
     params += f" --video-every-n-episodes={args.video_every_n_episodes}"
